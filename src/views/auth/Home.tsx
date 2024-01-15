c805d028dc14329ab352fa0c33e77cca3452e21d
Ajayi favour <ajayifavour81@gmail.com>
2024-01-15 23:29:50 +0100

    Trackplayer setup

--- src/views/auth/Home.tsx
+++ src/views/auth/Home.tsx
@@ -14,6 +14,7 @@ import {getClient} from '@src/api/Client';
 import {useFetchPlaylist} from '@src/hooks/query';
 import {updateNotification} from '@src/store/notification';
 import TrackPlayer, { Track } from 'react-native-track-player';
+import useAudioController from '@src/hooks/useAudioController';
 
 interface Props {}
 
@@ -22,6 +23,7 @@ const Home: FC<Props> = props => {
   const [selectedAudio, setSelectedAudio] = useState<AudioData>();
   const [showPlaylistModal, setShowPlaylistModal] = useState(false);
   const [showPlaylistForm, setShowPlaylistForm] = useState(false);
+  const {onAudioPress} = useAudioController()
 
   const {data} = useFetchPlaylist();
 
@@ -102,27 +104,11 @@ const Home: FC<Props> = props => {
   return (
     <View style={styles.container}>
       <LatestUploads
-        onAudioPress={async(item, data) => {
-          const lists: Track[] = data.map(item => {
-            return {
-              id: item.id,
-              title: item.title,
-              url: item.file,
-              artwork: item.poster || require('../../assets/music.png'),
-              artist: item.owner.name,
-              genre: item.category,
-              isLiveStream: true,
-            };
-          });
-          await TrackPlayer.add([...lists]);
-          await TrackPlayer.play();
-        }}
+        onAudioPress={onAudioPress}
         onAudioLongPress={handleOnLongPress}
       />
       <RecommendedAudios
-        onAudioPress={item => {
-          console.log(item);
-        }}
+        onAudioPress={onAudioPress}
         onAudioLongPress={handleOnLongPress}
       />
       <OptionsModal
